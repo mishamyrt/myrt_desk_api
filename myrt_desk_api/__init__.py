c288643dd27f189276579c8a98ec0d47d7907e4c
mishamyrt <misha@myrt.co>
2022-11-19 15:45:26 +0300

    Bump

--- myrt_desk_api/__init__.py
+++ myrt_desk_api/__init__.py
@@ -5,7 +5,7 @@ from .system import MyrtDeskSystem
 from .legs import MyrtDeskLegs
 from .discover import discover
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 class MyrtDesk:
     """MyrtDesk controller entity"""
