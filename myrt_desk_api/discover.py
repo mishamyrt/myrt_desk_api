4cbf87cb5a26337599ba304b7ed1fdf57587e955
mishamyrt <misha@myrt.co>
2023-01-06 19:37:07 +0300

    Remove dicovery–related helpers

--- myrt_desk_api/discover.py
+++ myrt_desk_api/discover.py
@@ -1,7 +1,6 @@
 """MyrtDesk discovery helper"""
 from typing import Union
 from asyncio import exceptions, wait_for, sleep
-from socket import getfqdn, gethostname, gethostbyname_ex
 from .datagram import open_broadcast, open_endpoint, Endpoint
 from .system.constants import DOMAIN_SYSTEM, COMMAND_PING
 from .constants import API_PORT
@@ -13,30 +12,6 @@ PING_REQUEST = bytes([
     DOMAIN_SYSTEM,
     COMMAND_PING
 ])
-
-def apply_mask(local_address: str, mask: str) -> str:
-    """Applies the netmask to the /24 address"""
-    segments = local_address.split(".")
-    mask_segments = mask.split(".")
-    result_address = ""
-    index = 0
-    for segment in mask_segments:
-        if len(result_address) > 0:
-            result_address += "."
-        if segment == "255":
-            result_address += f"{segments[index]}"
-        elif segment == "0":
-            result_address += "255"
-        index += 1
-    return result_address
-
-def get_discovery_host() -> str:
-    """Finds discovery host"""
-    hosts = gethostbyname_ex(getfqdn(gethostname()))
-    local_ips = hosts[2]
-    if len(local_ips) > 1:
-        return ""
-    return apply_mask(local_ips[0], "255.255.255.0")
 
 async def discover(host: str = None) -> Union[None, str]:
     """Finds desk IP address on network via broadcast ping"""
